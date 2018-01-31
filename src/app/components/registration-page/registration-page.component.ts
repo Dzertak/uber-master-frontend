6e2f7069b17b561dea5dd016c6711473d40f82f5
Vitaly <meloman787@gmail.com>
2018-01-31 17:58:58 +0200

    Register validation improvement

--- src/app/components/registration-page/registration-page.component.ts
+++ src/app/components/registration-page/registration-page.component.ts
@@ -1,15 +1,20 @@
 import {Component, NgModule, OnInit} from '@angular/core';
-import {Router} from "@angular/router";
+import {Router} from '@angular/router';
 import * as  Cloudinary from 'cloudinary-core';
-import { FileUploader } from 'ng2-file-upload';
-import {RegistrationService} from "../../services/registration.service";
-import {User} from "../../models/user.model";
-import {Poke} from "../../models/poke.model";
-import {Master} from "../../models/master.model";
+import {FileUploader} from 'ng2-file-upload';
+import {RegistrationService} from '../../services/registration.service';
+import {User} from '../../models/user.model';
+import {Poke} from '../../models/poke.model';
+import {Master} from '../../models/master.model';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {OrderService} from '../../services/order.service';
+import {Order} from '../../models/order.model';
+import {AuthorizeService} from '../../services/authorize.service';
+import {isBoolean} from 'util';
 
 const URL = 'https://api.cloudinary.com/v1_1/ubermaster/image/upload';
 
-declare var $:any;
+declare var $: any;
 
 @Component({
   selector: 'app-registration-page',
@@ -17,63 +22,91 @@ declare var $:any;
   styleUrls: ['./registration-page.component.css']
 })
 export class RegistrationPageComponent implements OnInit {
-  public uploader:FileUploader = new FileUploader({url: URL});
-  swipe: number = 0
+  public uploader: FileUploader = new FileUploader({url: URL});
+  swipe: number = 0;
   picturePoke: string;
-  firstNamePoke: string;
-  lastNamePoke: string;
-  phoneNumberPoke: string;
-  locationPoke: string;
-  userDescriptionPoke: string;
-  passwordPoke: string;
-  confirmPasswordPoke: string;
 
   pictureMaster: string;
-  firstNameMaster: string;
-  lastNameMaster: string;
-  phoneNumberMaster: string;
-  locationMaster: string;
-  userDescriptionMaster: string;
-  passwordMaster: string;
-  confirmPasswordMaster: string;
-  profession: string;
-  tools: string;
-  skills: string;
-  experience: string;
-  startTime: number;
-  endTime: number;
-  payment: number;
-  smoke: boolean;
+
+  variable: { phoneNumber, pass }
+
+  private order: Order;
+  public rPokeForm: FormGroup;
+  public rMasterForm: FormGroup;
+  private create: any;
+  public locations: { id: number; name: string }[];
+  public professions: { id: number; name: string }[];
+
   averMark: number;
+  smoke: boolean;
 
-  variable: {phoneNumber, pass}
 
-  constructor(private router: Router, private registrationService: RegistrationService) {}
+  constructor(private fb: FormBuilder, private router: Router, private registrationService: RegistrationService) {
 
-  ngOnInit() {
+    this.rPokeForm = fb.group({
+      'firstNamePoke': [null, Validators.required],
+      'lastNamePoke': [null, Validators.required],
+      'phoneNumberPoke': [null, Validators.pattern('^380[0-9]{9}$')],
+      'locationPoke': ['Location...', Validators.required],
+      'userDescriptionPoke': [null],
+      'passwordPoke': [null, Validators.required],
+      'confirmPasswordPoke': [null, Validators.required]
+    })
+
+    this.rMasterForm = fb.group({
+      'firstNameMaster': [null, Validators.required],
+      'lastNameMaster': [null, Validators.required],
+      'phoneNumberMaster': [null, Validators.pattern('^380[0-9]{9}$')],
+      'locationMaster': ['Location...', Validators.required],
+      'userDescriptionMaster': [null],
+      'passwordMaster': [null, Validators.required],
+      'confirmPasswordMaster': [null, Validators.required],
+      'profession': ['Profession...', Validators.required],
+      'startTime': [null, Validators.required],
+      'endTime': [null, Validators.required],
+      'tools': [null],
+      'experience': [null, Validators.required],
+      'skills': [null],
+      'smoke': [null, Validators.required],
+      'payment': [null, Validators.required]
+
+    })
+
   }
 
-  authorization(){
+  ngOnInit() {
+    this.locations = [
+      {id: 0, name: 'Location...'},
+      {id: 1, name: 'Primorskyy'},
+      {id: 2, name: 'Malinovskyy'},
+      {id: 3, name: 'Kievskyy'},
+      {id: 4, name: 'Suvorovskyy'},
+    ];
+
+    this.professions = [
+      {id: 0, name: 'Profession...'},
+      {id: 1, name: 'Locksmith'},
+      {id: 2, name: 'Electrician'},
+      {id: 3, name: 'Cleaner'},
+      {id: 4, name: 'Computer foreman'},
+      {id: 4, name: 'Handyman'}
+    ];
+  }
+
+  authorization() {
     this.router.navigate(['authorization']);
   }
 
-  swipeTab(tab: number){
+  swipeTab(tab: number) {
     this.swipe = tab;
   }
 
-  signUpPoke() {
-    if (this.firstNamePoke != "" && this.lastNamePoke != "" && this.phoneNumberPoke != ""
-      && this.passwordPoke != "" && this.confirmPasswordPoke != "" && this.locationPoke != "") {
-      if (this.passwordPoke == this.confirmPasswordPoke) {
-        let poke = new Poke(this.firstNamePoke + " " + this.lastNamePoke, null, null, this.locationPoke,
-          this.userDescriptionPoke, this.phoneNumberPoke, this.passwordPoke, this.picturePoke, false);
-        this.registrationService.reg(poke,'Poke');
-      } else {
-        //error
-      }
-    } else {
-      //error
-    }
+  signUpPoke(create) {
+    let poke = new Poke(create.firstNamePoke + ' ' + create.lastNamePoke,
+      null, null, create.locationPoke, create.userDescriptionPoke,
+      create.phoneNumberPoke, create.passwordPoke, ' ', false);
+    this.registrationService.reg(poke, 'Poke');
+
     /*.subscribe(response => {
       if (response.status==200){
         this.router.navigate(['authorization']);
@@ -81,26 +114,26 @@ export class RegistrationPageComponent implements OnInit {
     });*/
   }
 
-  signUpMaster(){
-    if (this.firstNameMaster != "" && this.lastNameMaster != "" && this.phoneNumberMaster != ""
-      && this.passwordMaster != "" && this.confirmPasswordMaster != "" && this.locationMaster != ""
-      && this.profession != "" && this.experience != "") {
-      if (this.passwordMaster == this.confirmPasswordMaster) {
-        let master = new Master(this.firstNameMaster + " " + this.lastNameMaster, null, null, this.locationMaster,
-          this.userDescriptionMaster, this.phoneNumberMaster, this.passwordMaster, this.pictureMaster, false,
-          this.profession, this.skills, this.experience, this.payment, this.smoke, this.tools, this.startTime, this.endTime, this.averMark);
-        this.registrationService.reg(master,'Master');
-      } else {
-        //error
-      }
+  signUpMaster(create) {
+    if (create.smoke == 'true') {
+      this.smoke = true;
     } else {
-      //error
+      this.smoke = false;
     }
+
+    let master = new Master(create.firstNameMaster + ' ' + create.lastNameMaster,
+      null, null, create.locationMaster, create.userDescriptionMaster,
+      create.phoneNumberMaster, create.passwordMaster, create.pictureMaster, false,
+      create.profession, create.skills, create.experience, create.payment, create.smoke,
+      create.tools, create.startTime, create.endTime, create.averMark);
+    this.registrationService.reg(master, 'Master');
   }
 
-  uploadImage(nameImage: String){
+  uploadImage(nameImage: String) {
     this.uploader.autoUpload(nameImage,
-      function(error, result) {console.log(result); });
+      function (error, result) {
+        console.log(result);
+      });
   }
 
 }
